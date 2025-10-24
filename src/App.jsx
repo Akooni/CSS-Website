20f00566d4172352e3736df7077ba743ab22c0d1
Akooni <127095348+Akooni@users.noreply.github.com>
2025-10-24 14:46:09 +0400

    added name function

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Welcome ({name}) {
+  return <h2>Welcome, {name}!</h2>
+}
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +14,8 @@ function App() {
   return (
       <main style= {{ fontFamily: "system-ui", padding: 24}}>
         <h1>Hello, React on windows!!</h1>
-        <p>Clicks and kicks: {count}</p>
+        <Welcome name="Sufyan"/>
+        <p>Clicks and kicks and pips: {count}</p>
         <button onClick={() => setCount((count) => count + 1)}>click me </button>
       </main>
   );
